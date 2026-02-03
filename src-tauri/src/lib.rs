9eaa5d9bc7cb9f00ec84564be44425962ab0bc5e
Vincent <puterich@gmail.com>
2026-02-03 20:42:51 +0100

    fix plugin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -8,9 +8,12 @@ pub fn run() {
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_dialog::init())
-        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_process::init())
         .setup(|app| {
+            #[cfg(desktop)]
+            app.handle()
+                .plugin(tauri_plugin_updater::Builder::new().build())?;
+
             // Start the sidecar on app startup
             let app_handle = app.handle().clone();
             tauri::async_runtime::spawn(async move {
